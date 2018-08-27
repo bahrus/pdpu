bb40818cc834e44235fdc6ead1bd8f1b4fc1a554
bahrus <anderson.bruce.b@gmail.com>
2018-08-27 19:09:34 -0400

    ...

---
+++ build/ES6/p-s.js
@@ -0,0 +1,1 @@
+import{PDX}from"./p-d-x.js";import{define}from"./node_modules/xtal-latx/define.js";export class PS extends PDX{static get is(){return"p-s"}pass(e){this.passDown(e.target,e,0)}}define(PS);
